b226ff32a44e563c993143cdfca11d3d1f021c16
Agent C1 <agentc1@redshift.is>
2025-11-29 00:42:00 +0000

    updates

--- config/common_config.ads
+++ config/common_config.ads
@@ -15,6 +15,6 @@ package Common_Config is
    Alire_Host_Distro : constant String := "fedora";
 
    type Build_Profile_Kind is (release, validation, development);
-   Build_Profile : constant Build_Profile_Kind := development;
+   Build_Profile : constant Build_Profile_Kind := release;
 
 end Common_Config;
