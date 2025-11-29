1c0f550d92a14b81c4e248eef7de9fcbb3faf093
Agent C1 <agentc1@redshift.is>
2025-11-29 00:38:39 +0000

    remove unneeded cruft

--- config/common_config.ads
+++ config/common_config.ads
@@ -12,9 +12,9 @@ package Common_Config is
 
    Alire_Host_Arch : constant String := "aarch64";
 
-   Alire_Host_Distro : constant String := "ubuntu";
+   Alire_Host_Distro : constant String := "fedora";
 
    type Build_Profile_Kind is (release, validation, development);
-   Build_Profile : constant Build_Profile_Kind := release;
+   Build_Profile : constant Build_Profile_Kind := development;
 
 end Common_Config;
--- config/common_config.h
+++ config/common_config.h
@@ -9,12 +9,12 @@
 
 #define ALIRE_HOST_ARCH "aarch64"
 
-#define ALIRE_HOST_DISTRO "ubuntu"
+#define ALIRE_HOST_DISTRO "fedora"
 
 #define BUILD_PROFILE_RELEASE 1
 #define BUILD_PROFILE_VALIDATION 2
 #define BUILD_PROFILE_DEVELOPMENT 3
 
-#define BUILD_PROFILE  1
+#define BUILD_PROFILE  3
 
 #endif
